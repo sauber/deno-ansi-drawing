bb4f4bfc3d79b59f734608d02efbd834ae02f24e
Søren Døssing <netcom@sauber.net>
2026-05-19 17:12:01 +0200

    Missing corners

--- src/frame.test.ts
+++ src/frame.test.ts
@@ -13,3 +13,58 @@ Deno.test("drawFrame minimum size", () => {
   const canvas: Canvas = new CharCanvas();
   assertThrows(() => drawFrame(canvas, 0, 0, 1, 1));
 });
+
+Deno.test("Float#1", () => {
+  const position = [
+    14.669710012050714,
+    5.466408422492504,
+    25.238675876884326,
+    8.903691876313028,
+  ];
+  const canvas: Canvas = new CharCanvas();
+  drawFrame(
+    canvas,
+    position[0],
+    position[1],
+    position[2] - position[0],
+    position[3] - position[1],
+  );
+  // console.log(canvas.toString());
+  assertEquals(
+    canvas.toString(),
+    [
+      "┌────────┐",
+      "│        │",
+      "│        │",
+      "└────────┘",
+    ].join("\n"),
+  );
+});
+
+Deno.test("Float#2", () => {
+  const position = [
+    46.64728588917686,
+    17.710518022841903,
+    52.67268232266839,
+    23.22815091068719,
+  ];
+  const canvas: Canvas = new CharCanvas();
+  drawFrame(
+    canvas,
+    position[0],
+    position[1],
+    position[2] - position[0],
+    position[3] - position[1],
+  );
+  // console.log(canvas.toString());
+  assertEquals(
+    canvas.toString(),
+    [
+      "┌────┐",
+      "│    │",
+      "│    │",
+      "│    │",
+      "└────┘",
+    ].join("\n"),
+  );
+});
--- src/frame.ts
+++ src/frame.ts
@@ -20,14 +20,6 @@ export function drawFrame(
   if (width < 2 || height < 2) {
     throw new Error("Width and height must be at least 2 to draw a frame.");
   }
-  // Top left corner
-  canvas.insert(x, y + height - 1, "┌", ansi);
-  // Top right corner
-  canvas.insert(x + width - 1, y + height - 1, "┐", ansi);
-  // Bottom right corner
-  canvas.insert(x + width - 1, y, "┘", ansi);
-  // Bottom left corner
-  canvas.insert(x, y, "└", ansi);
   // Top and bottom edges
   for (let i = 1; i < width - 1; i++) {
     canvas.insert(x + i, y + height - 1, "─", ansi);
@@ -38,4 +30,12 @@ export function drawFrame(
     canvas.insert(x, y + i, "│", ansi);
     canvas.insert(x + width - 1, y + i, "│", ansi);
   }
+  // Top left corner
+  canvas.insert(x, y + height - 1, "┌", ansi);
+  // Top right corner
+  canvas.insert(x + width - 1, y + height - 1, "┐", ansi);
+  // Bottom right corner
+  canvas.insert(x + width - 1, y, "┘", ansi);
+  // Bottom left corner
+  canvas.insert(x, y, "└", ansi);
 }
